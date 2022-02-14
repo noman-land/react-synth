e7baa0c92e6978f0cf84ad5b14fe1067cf8d6d0e
Noman <noman@noman.land>
2022-02-13 22:42:11 -0500

    Remove unneeded state

--- src/keyboard/useSynth.js
+++ src/keyboard/useSynth.js
@@ -1,20 +1,15 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useRef } from 'react';
 import * as Tone from 'tone';
 
 export const useSynth = () => {
   const synth = useRef();
-  const [started, setStarted] = useState(false);
 
-  const onPlayNote = useCallback(
-    async ({ target: { value: note } }) => {
-      if (!started) {
-        await Tone.start();
-        synth.current = new Tone.Synth().toDestination();
-        setStarted(true);
-      }
-      synth.current.triggerAttackRelease(note, '8n');
-    },
-    [started]
-  );
+  const onPlayNote = useCallback(async ({ target: { value: note } }) => {
+    if (!synth.current) {
+      await Tone.start();
+      synth.current = new Tone.Synth().toDestination();
+    }
+    synth.current.triggerAttackRelease(note, '8n');
+  }, []);
   return { onPlayNote };
 };
